2a7de43347f5dbee7b1f4383e6b1935a46974977
Andrew Carroll <andrewjcarroll3@gmail.com>
2017-12-06 19:50:00 +0000

    Added a login and logout

--- express_server.js
+++ express_server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
 
-var app = express();
+let app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 var PORT = process.env.PORT || 8080; // default port 8080
@@ -13,7 +14,7 @@ let urlDatabase = {
 };
 
 // Generates a 6 character random string based on character set
-function generateRandomString() {
+var generateRandomString = function() {
   let legend = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let shortURL = '';
 
@@ -27,24 +28,32 @@ app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 
 app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase};
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.headers['cookie']
+  }
+
   res.render("urls_index", templateVars);
 });
 
 app.post('/urls', (req, res) => {
-  console.log(req.body);
   var shortUrl = generateRandomString();
   urlDatabase[shortUrl] = req.body.longURL;
+
   res.redirect(303, `/urls/${shortUrl}`);   // Redirect to the generated short url after a form submission
 });
 
 app.delete('/urls/:id/delete', (req, res) => {
   delete urlDatabase[req.params.id];
-  res.redirect('/urls');   // Redirect to the generated short url after a form submission
+  res.redirect('/urls');
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.headers['cookie']
+  }
+
+  res.render('urls_new', templateVars);
 });
 
 app.get('/u/:id', (req, res) => {
@@ -55,14 +64,30 @@ app.get('/u/:id', (req, res) => {
   }
 });
 
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
 app.put('/urls/:id', (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
+
   res.redirect(`/urls/${req.params.id}`);   // Redirect to the generated short url after a form submission
 });
 
 app.get('/urls/:id', (req, res) => {
   if (urlDatabase.hasOwnProperty(req.params.id)) {
-    let templateVars = { shortURL: req.params.id, urls: urlDatabase };
+    let templateVars = {
+      shortURL: req.params.id,
+      urls: urlDatabase,
+      username: req.headers['cookie']
+    }
+
     res.render('urls_show', templateVars);
   } else {
     res.sendStatus(404).end();    // Send a 404 response when the short url is not in the database
