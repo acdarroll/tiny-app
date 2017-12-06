cce4b71263ea8f943f7d86c13470701a980e83ab
Andrew Carroll <andrewjcarroll3@gmail.com>
2017-12-06 16:49:48 +0000

    Added delete buttons to index for each url

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
-var express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
 
 var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -7,13 +8,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 var PORT = process.env.PORT || 8080; // default port 8080
 
 let urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com'
 };
 
 // Generates a 6 character random string based on character set
 function generateRandomString() {
-  let legend = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let legend = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let shortURL = '';
 
   while(shortURL.length < 6) {
@@ -22,26 +23,31 @@ function generateRandomString() {
   return shortURL;
 }
 
-
+app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 
-app.get("/urls", (req, res) => {
+app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
 
-app.post("/urls", (req, res) => {
+app.post('/urls', (req, res) => {
   console.log(req.body);
   var shortUrl = generateRandomString();
   urlDatabase[shortUrl] = req.body.longURL;
   res.redirect(303, `/urls/${shortUrl}`);   // Redirect to the generated short url after a form submission
 });
 
-app.get("/urls/new", (req, res) => {
+app.delete('/urls/:id/delete', (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect('/urls');   // Redirect to the generated short url after a form submission
+});
+
+app.get('/urls/new', (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/u/:id", (req, res) => {
+app.get('/u/:id', (req, res) => {
   if (urlDatabase.hasOwnProperty(req.params.id)) {
     res.redirect(307, urlDatabase[req.params.id]);  // Redirect to the longurl of the short url
   } else {
@@ -49,10 +55,10 @@ app.get("/u/:id", (req, res) => {
   }
 });
 
-app.get("/urls/:id", (req, res) => {
+app.get('/urls/:id', (req, res) => {
   if (urlDatabase.hasOwnProperty(req.params.id)) {
     let templateVars = { shortURL: req.params.id, urls: urlDatabase };
-    res.render("urls_show", templateVars);
+    res.render('urls_show', templateVars);
   } else {
     res.sendStatus(404).end();    // Send a 404 response when the short url is not in the database
   }
