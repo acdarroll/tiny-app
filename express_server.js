e5a1c2a1d42438b7b6757daab172fdc8986208ae
Andrew Carroll <andrewjcarroll3@gmail.com>
2017-12-07 00:12:04 +0000

    Customized landing page to only include user urls

--- express_server.js
+++ express_server.js
@@ -22,12 +22,20 @@ const users = {
 }
 
 const urlDatabase = {
-  'b2xVn2': 'http://www.lighthouselabs.ca',
-  '9sm5xK': 'http://www.google.com'
+  'b2xVn2': {
+    userId: 'h2h',
+    shortUrl: 'b2xVn2',
+    longUrl: 'http://www.lighthouselabs.ca'
+  },
+  '9sm5xK': {
+    userId: 'brb',
+    shortUrl: '9sm5xK',
+    longUrl: 'http://www.google.com'
+  }
 };
 
 // Generates a 6 character random string based on character set
-var generateRandomString = function() {
+let generateRandomString = function() {
   let legend = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let shortURL = '';
 
@@ -37,14 +45,30 @@ var generateRandomString = function() {
   return shortURL;
 }
 
+let urlsForUser = function(id) {
+  let userUrls = [];
+  for(var url in urlDatabase) {
+    if(urlDatabase[url].userId === id) {
+      userUrls.push(urlDatabase[url]);
+    }
+  }
+  return userUrls;
+}
+
+
 app.use(methodOverride('_method'));
 app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 /*  INDEX PAGE  */
 app.get('/urls', (req, res) => {
+  let userUrls = [];
+  if(req.cookies['user_id']) {
+    userUrls = urlsForUser(req.cookies['user_id']);
+  }
+  console.log(userUrls);
   let templateVars = {
-    urls: urlDatabase,
+    urls: userUrls,
     user: users[req.cookies['user_id']]
   }
 
@@ -52,8 +76,14 @@ app.get('/urls', (req, res) => {
 });
 
 app.post('/urls', (req, res) => {
-  var shortUrl = generateRandomString();
-  urlDatabase[shortUrl] = req.body.longURL;
+  let shortUrl = generateRandomString();
+  let userId = req.cookies['user_id'];
+
+  urlDatabase[shortUrl] = {
+    userId: userId,
+    shortUrl: shortUrl,
+    longUrl: req.body.longURL
+  }
 
   res.redirect(303, `/urls/${shortUrl}`);   // Redirect to the generated short url after a form submission
 });
@@ -112,8 +142,6 @@ app.get('/login', (req, res) => {
 
 app.post('/login', (req, res) => {
   for(var user in users) {
-    console.log("Input name:", req.body)
-    console.log("Stored names:", users[user])
     if(users[user].email === req.body.email) {
       if(users[user].password === req.body.password) {
         res.cookie('user_id', users[user].id);
@@ -134,7 +162,7 @@ app.post('/logout', (req, res) => {
 /*  REDIRECT WEBSITE  */
 app.get('/u/:id', (req, res) => {
   if (urlDatabase.hasOwnProperty(req.params.id)) {
-    res.redirect(307, urlDatabase[req.params.id]);  // Redirect to the longurl of the short url
+    res.redirect(307, urlDatabase[req.params.id].longUrl);  // Redirect to the longurl of the short url
   } else {
     res.redirect(307, '/urls/new');   // Redirect to the form submission if the short url does not exist
   }
@@ -142,16 +170,21 @@ app.get('/u/:id', (req, res) => {
 
 /*  URL SHORT URLS  */
 app.put('/urls/:id', (req, res) => {
-  urlDatabase[req.params.id] = req.body.longURL;
+  if(urlDatabase[req.params.id].userId === req.cookies['user_id']) {
+    urlDatabase[req.params.id].longUrl = req.body.longURL;
 
   res.redirect(`/urls/${req.params.id}`);   // Redirect to the generated short url after a form submission
+  } else {
+    res.redirect('/urls');
+  }
+
 });
 
 app.get('/urls/:id', (req, res) => {
   if (urlDatabase.hasOwnProperty(req.params.id)) {
     let templateVars = {
-      shortURL: req.params.id,
-      urls: urlDatabase,
+      shortUrl: req.params.id,
+      url: urlDatabase[req.params.id],
       user: users[req.cookies['user_id']]
     }
 
@@ -162,7 +195,9 @@ app.get('/urls/:id', (req, res) => {
 });
 
 app.delete('/urls/:id/delete', (req, res) => {
-  delete urlDatabase[req.params.id];
+  if(urlDatabase[req.params.id].userId === req.cookies['user_id']) {
+    delete urlDatabase[req.params.id];
+  }
   res.redirect('/urls');
 });
 
